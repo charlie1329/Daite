00f7f940b47e1a053f4e2e063f8485869ab083b4
George Sabourin <gcs516@student.bham.ac.uk>
2016-08-12 21:41:22 +0100

    Added chat window with animation + links, but still need to clear up slightly messy code

--- frontend-display/src/jquery.main.js
+++ frontend-display/src/jquery.main.js
@@ -1,6 +1,27 @@
 /**
  * Created by Artur on 09.08.2016.
+ * George (12.8) - May need to clear this up later, I just used this file to test some of the additional JQuery featurse
  */
+
+var username;
+
+function sendMessage() {
+    
+    var $client_message = "<li class='message'> <span class='user-name me''>" +
+                                username +
+                                "</span> <span class='message-text'>" +
+                                $('#type-message').val() +
+                                "</span> </li>";
+
+     // Clear text entry
+     $('#type-message').val("");
+
+     // Add client message to window
+     $('.message-list').append($client_message)
+    $('.chat-window').prop("scrollHeight")
+
+
+}
 
 (function ($) {
     $.get('views/enter-name.html', function (enter_name) {
@@ -9,9 +30,36 @@
         $('.enter-name .btn').click(function (event) {
             event.preventDefault();
             $('.enter-name .btn').prop('disabled', true).html('Loading...');
+            username = $('#name').val();
             setTimeout(function () {
-                $('.enter-name').slideUp()
+                $('.enter-name').slideUp();
             }, 1500); //simulate connecting with back-end
-        })
-    })
+            setTimeout(function () {
+                $('.chat-section').slideDown();
+            }, 2500);
+        });
+     })
 }(jQuery));
+
+$(document).ready(function() {
+    $('#type-message').keydown(function(ee) {
+        if(ee.keyCode == 13) {
+            sendMessage();
+        }
+    }
+)});
+
+
+
+function endConvo() {
+    confirm("You are about to end the conversation.");
+}
+
+$('#end').click(endConvo);
+
+var start = new Date;
+
+setInterval(function() {
+    $('#timer').text(Math.floor((new Date - start) / 1000) + " Seconds");
+}, 1000);
+
