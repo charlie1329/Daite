73aa5705cf1eeb77a1d7811671cffa446c6e017e
George Sabourin <gcs516@student.bham.ac.uk>
2016-08-15 17:22:37 +0100

    Added basic networking capability (need to communicate with networking branch), also added error when no name entered, and info dialogue

--- frontend-display/src/jquery.main.js
+++ frontend-display/src/jquery.main.js
@@ -1,44 +1,81 @@
 /**
  * Created by Artur on 09.08.2016.
- * George (12.8) - May need to clear this up later, I just used this file to test some of the additional JQuery featurse
  */
 
+/* Variables */
 var username;
+var socket = io.connect("http://localhost:6969/chat")
 
-function sendMessage() {
-    
-    var $client_message = "<li class='message'> <span class='user-name me''>" +
-                                username +
-                                "</span> <span class='message-text'>" +
-                                $('#type-message').val() +
-                                "</span> </li>";
-
-     // Clear text entry
-    $('#type-message').val("");
-
-     // Add client message to window
-    $('.message-list').append($client_message);
-    $('.chat-window').prop("scrollHeight")
-
-
-}
-
+/* ON LOAD */
 $(document).ready(function() {
+    
+    // Send message when enter pressed in text box
     $('#type-message').keydown(function(ee) {
         if(ee.keyCode == 13) {
             sendMessage();
         }
-    }
-)});
+    })
+    
+    // Show about dialogue
+    $('.about-button').click(function() {
+        $('.about-dialogue').slideDown();
+    })
+    
+    // Hide about dialogue
+    $('.close').click(function() {
+        $('.about-dialogue').slideUp();
+    })
+    
+    $('#end').click(endConvo)
+})
 
+/* Sending and Receiving Messages */
+socket.on("message", function(data) {
+    if(data.username != username) {
+        receiveMessage(data);
+    }
+})
+
+function receiveMessage(data) {
+    // Get username and message
+    var match_username = data.username;
+    var match_message = data.message;
+    
+    // Add message to the chat window
+    var received_message = "<li class='message'> <span class='user-name match'>" +
+                                match_username +
+                                "</span> <span class='message-text'>" +
+                                match_message +
+                                "</span> </li>";
+    $('.message-list').append(received_message);
+}
+
+function sendMessage() { 
+    // Add message to the chat window
+    var client_message = "<li class='message'> <span class='user-name me''>" +
+                                username +
+                                "</span> <span class='message-text'>" +
+                                $('#type-message').val() +
+                                "</span> </li>";
+    $('.message-list').append(client_message);
+    
+    // Clear text entry
+    $('#type-message').val("");
+
+    socket.send({username:username, message:client_message})  
+}
+
+/* MISC FUNCTIONS */
 function endConvo() {
     confirm("You are about to end the conversation.");
 }
 
-$('#end').click(endConvo);
+// Toggle the typing indicator 
+function typingIndicator() {
+    
+}
 
-// Main stuff happens below!!!
-
+/* Main stuff happens below!!! */
 (function ($) {
     var view_enter_name = $.get('views/enter-name.html', function (enter_name) {
         $('.content').append(enter_name);
@@ -47,17 +84,27 @@ $('#end').click(endConvo);
     var view_chat_section = $.get('views/chat-section.html', function (chat_section) {
         $('.content').append(chat_section);
     });
+    
+    var view_chat_section = $.get('views/about-float.html', function (about_float) {
+        $('.content').append(about_float);
+    });
 
     $.when(view_enter_name, view_chat_section).done(function () {
         $('.enter-name').slideDown();
         $('.enter-name .btn').click(function (event) {
+            
+            if(($('#name').val()) == "") {
+               alert("Error: No name has been entered!");
+            }
+            else {
             event.preventDefault();
             $('.enter-name .btn').prop('disabled', true).html('Loading...');
             username = $('#name').val();
             setTimeout(function () {
                 $('.enter-name').slideUp();
             }, 1000); //simulate connecting with back-end
-            setTimeout(showChat, 2000);
+            setTimeout(showChat, 2000);  
+            }
         });
 
         function showChat () {
