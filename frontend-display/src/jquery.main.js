26a429400f3e2d2bd2c368327dc882419ce7ac03
George Sabourin <gcs516@student.bham.ac.uk>
2016-08-19 21:01:25 +0100

    Changed networking appraoch to add rooms, add auto focus to text boxes, added no-age/no-gender prompts and started work on typing prompt

--- frontend-display/src/jquery.main.js
+++ frontend-display/src/jquery.main.js
@@ -4,13 +4,43 @@
 
 /* Variables */
 var my_username;
-var socket = io.connect("http://duk.im:6969/chat");
+var my_age;
+var my_gender; 
+
+var typing = false;
+
+
+/* TYPING -- MOVE ME LATER */
+
+function typingTimeout() {  
+  typing = false;
+  socket.emit("typing", false);
+}
+
+// If receives an isTyping from the socket, toggles the typing display
+socket.on("isTyping", function(data) {  
+    if (data.isTyping) {
+        $('.typing-indicator').slideDown;
+    }
+    else {
+        $('.typing-indicator').slideUp;
+    }
+});
+
+/// STILL TO BE CONTINUED -- BASING UPON http://www.tamas.io/further-additions-to-the-node-jssocket-io-chat-app/
+
+
+//var socket = io.connect("http://duk.im:6969/chat");
+var socket = io.connect("http://localhost:6969/chat");
+
 
 /* ON LOAD */
 $(document).ready(function () {
+    
+    // Brings focus to name entry text box on page load
+    $("#name").focus();
 
     // Define the dialogs
-
     var content = $('.content'),
         warning_dialog = $('#warning-dialog'),
         yes_no_dialog = $('#yes-no-dialog');
@@ -74,6 +104,7 @@ $(document).ready(function () {
         $('.about-dialogue').slideUp();
     });
 
+    // End conversation dialogue
     $('#end').click(function () {
             yes_no_dialog.dialog('option', 'title', 'End conversation')
                 .text("Are you sure you want to end this conversation early?")
@@ -164,6 +195,9 @@ function sendMessage() {
 
 /* MISC FUNCTIONS */
 function endConvo() {
+    // Leave the room
+    socket.emit("leaveroom", "test");
+    
     $('.chat-section').slideUp();
     $('#warning-dialog').dialog('option', 'title', "It's over!")
         .text("Time's up, the conversation is now finished.")
@@ -190,6 +224,8 @@ function typingIndicator() {
 
 /* Main stuff happens below!!! */
 (function ($) {
+    
+    // Load external views
     var view_enter_name = $.get('views/enter-name.html', function (enter_name) {
         $('.content').append(enter_name);
     });
@@ -205,9 +241,11 @@ function typingIndicator() {
     var about_float = $.get('views/about-float.html', function (about_float) {
         $('.content').append(about_float);
     });
+    
 
     $.when(view_enter_name, view_chat_section, about_float).done(function () {
         $('.enter-name').slideDown();
+        
         $('.enter-name .btn').click(function (event) {
             event.preventDefault();
             my_username = $('#name').val();
@@ -224,26 +262,59 @@ function typingIndicator() {
                     .dialog('open')
         });
         
+        // Show details entering view
         function showDetails() {
             $('.enter-details').slideDown();
             
-            $('.enter-details .btn').click(function (event) {
-            event.preventDefault();
-            my_age = $('#age').val();
-            my_gender = $('input[name=gender]:checked', '#gender-form').val()
-            my_gender = $('input[name=gender]:checked').val();
-    
-            $('.enter-details .btn').prop('disabled', true).html('Loading...');
-            setTimeout(function () {
-                $('.enter-details').slideUp();
-            }, 500); //simulate connecting with back-end
-                setTimeout(showChat, 1500);
-            });
+            // Brings focus to age text box as soon as page loads
+            $("#age").focus(); 
+
+            $('.enter-details .btn').click(function (event) {  
+                
+                event.preventDefault();
+                
+                // Get entered age
+                my_age = $('#age').val();
+                
+                // Get gender chosen
+                my_gender = $('input[name=gender]:checked').val();
+        
+                // Prompt if no age entered
+                if (my_age == null || !my_age) {
+                    $('#warning-dialog').dialog('option', 'title', 'Uh oh!')
+                        .text('You must enter your age to continue.')
+                        .dialog('open')
+                } 
+                
+                // Prompt if no gender selected
+                else if (my_gender == null || !my_age) {
+                    $('#warning-dialog').dialog('option', 'title', 'Uh oh!')
+                        .text('You must choose your gender to continue.')
+                        .dialog('open')
+                }
+                else {
+                    $('.enter-details .btn').prop('disabled', true).html('Loading...');
+                    setTimeout(function () {
+                        $('.enter-details').slideUp();
+                        }, 500); //simulate connecting with back-end
+                        setTimeout(showChat, 1500);
+                }             
+            })
         }
 
+        // Show chat view
         function showChat() {
             $('.chat-section').slideDown();
-
+            
+            // Bring focus to text-entry box
+            $("#type-message").focus(); 
+            
+            // Registers the specific client
+            socket.emit("register", {name: my_username, age: my_age, gender: my_gender});
+            socket.emit("joinroom", "test");
+            
+            
+            // Start timer
             var start = new Date;
             setInterval(function () {
 
