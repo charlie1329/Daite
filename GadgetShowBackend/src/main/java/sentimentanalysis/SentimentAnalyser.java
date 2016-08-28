eb3754e1cecc46a8c3ac520eeb911ce2c008ec86
IanTempleGH <ianrtemple@gmail.com>
2016-08-28 17:07:37 +0100

    Class for sentiment analysis
    This class will likely be the wrapper for creating a sentiment analyser
    from an external jar and building it with data

---
+++ GadgetShowBackend/src/main/java/sentimentanalysis/SentimentAnalyser.java
@@ -0,0 +1,11 @@
+package sentimentanalysis;
+
+/**this class will be solely the implementation of a sentiment analyser
+ * (working on the what & how). The idea is that i will then create an evaluator class
+ * which will utilise this and other factors to create a simple heuristic for response 
+ * searching. It will likely be used in other parts too.
+ * @author Ian Temple
+ */
+public class SentimentAnalyser {
+
+}
