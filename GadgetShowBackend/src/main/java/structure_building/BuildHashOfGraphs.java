3143ae3f27b585d485dd6136f73a1fc07805f9f4
charlie1329 <charlie.street1@btinternet.com>
2016-09-01 12:34:14 +0100

    Getting there with this data structure building

--- GadgetShowBackend/src/main/java/structure_building/BuildHashOfGraphs.java
+++ GadgetShowBackend/src/main/java/structure_building/BuildHashOfGraphs.java
@@ -1,9 +1,7 @@
 package structure_building;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
@@ -12,7 +10,6 @@ import java.util.concurrent.Executors;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import data_structures.*;
 import logger.ConvoLogger;
@@ -34,8 +31,54 @@ public class BuildHashOfGraphs {
 		return folder;
 	}
 	
+	/**method will take a question or response and organise the keywords into one convenient array
+	 * 
+	 * @param qOrR the object that is either a question or response
+	 * @return a string array of keywords
+	 */
+	private static String[] getKeywords(JSONObject qOrR) {
+		JSONArray presentKeywords = null;
+		JSONArray absentKeywords = null;
+		JSONArray negatedKeywords = null;
+		try {presentKeywords = (JSONArray)qOrR.get("presentKeywords");}catch(Exception e){}
+		try {absentKeywords = (JSONArray)qOrR.get("absentKeywords");}catch(Exception e) {}
+		try {negatedKeywords = (JSONArray)qOrR.get("negatedKeywords");}catch(Exception e) {}
+		
+		ArrayList<String> keyWords = new ArrayList<String>();//converting into array list of strings
+		if(presentKeywords != null) {
+			for(int i = 0; i < presentKeywords.size(); i++) {
+				keyWords.add((String)presentKeywords.get(i));
+			}
+		}
+		if(absentKeywords != null) {
+			for(int i = 0; i < absentKeywords.size(); i++) {
+				keyWords.add((String)absentKeywords.get(i));
+			}
+		}
+		if(negatedKeywords != null) {
+			for(int i = 0; i < negatedKeywords.size(); i++) {
+				keyWords.add((String)negatedKeywords.get(i));
+			}
+		}
+		
+		String[] keys = new String[keyWords.size()];//converting array list to standard array
+		keys = keyWords.toArray(keys);
+		return keys;
+	}
+	
+	/**method takes a list of children from a json file and forms it into a set of responses
+	 * 
+	 * @param responses the responses from the json file
+	 * @param parent the question those responses are suitable for
+	 * @return the responses in response objects
+	 */
+	private static ArrayList<Response> formResponses(JSONArray responses, Question parent) {
+		return new ArrayList<Response>();//fill in!
+	}
+	
 	/**this method will build up our hash map by building each topic in a separate thread using thread pools
 	 * 
+	 * @param logger our on-screen logger for how the AI is doing
 	 * @return the full data structure used for conversations
 	 */
 	public static HashMap<String,Question> build(ConvoLogger logger) {
@@ -58,12 +101,29 @@ public class BuildHashOfGraphs {
 						ArrayList<Response> topicRs = new ArrayList<Response>();
 						
 						for(int i = 0; i < questions.size(); i++) {//looping through all questions
+							JSONObject question = (JSONObject)questions.get(i);
+							String message = (String)question.get("message");//the actual question
 							
+							String[] keywords = getKeywords(question);//combine keywords to one array
+							
+							boolean usedByAI = false;//does the ai use this question?
+							try {usedByAI = (boolean)question.get("usedByAI");}catch(Exception e){}
+							
+							boolean isOpener = false;//is this the opening question
+							try {isOpener = (boolean)question.get("isOpener");}catch(Exception e){}
+							
+							Question newQuestion = new Question(message, keywords, isOpener, usedByAI);
+							if(newQuestion.isOpener()) {opener = newQuestion;}//should only happen once
+							topicQs.add(newQuestion);//add to list
+							
+							JSONArray responses = (JSONArray)question.get("children");//getting responses for question
+							topicRs.addAll(formResponses(responses,newQuestion));
 						}
 						
 						synchronized(convoMap) {//putting into synchronized block to prevent any issues
 							convoMap.put(topicName, opener);//with multiple threads modifying the same object
 						}
+						
 					} catch(Exception e) {//if something goes wrong parsing a JSON file
 						logger.logMessage("Error parsing JSON file: " + currentFile + "\n" +
 										  e.getMessage() + 
