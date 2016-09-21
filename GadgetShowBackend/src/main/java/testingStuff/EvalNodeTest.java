dd49656667d384885961f80a25e0d86b6828d536
tauomicronmu <tauomicronmurs@gmail.com>
2016-09-21 22:38:34 +0100

    worked out that capitals are bad

--- GadgetShowBackend/src/main/java/testingStuff/EvalNodeTest.java
+++ GadgetShowBackend/src/main/java/testingStuff/EvalNodeTest.java
@@ -12,17 +12,20 @@ public class EvalNodeTest
  {
 	Analyser analyser = new Analyser();
 
-	String heldMessage = "Do you like memes?";
+	String heldMessage = "do you like memes?";
 	String[] kWords = new String[2];
 	kWords[0] = "memes";
 	kWords[1] = "like";
 	ArrayList<String> neighbours = new ArrayList<String>();
 	String incQuestion = "do you like memes?";
+	String goodQuestion = "how much do you like memes?";
 	String avgQuestion = "what is your opinion on memes?";
 	String badQuestion = "how is your pet rhinocerous this fine morning?";
 	Question node = new Question(heldMessage, kWords, false, false, "test", analyser);
+		
 	System.out.println("Score: " + node.evaluate(badQuestion));
 	System.out.println("Score: " + node.evaluate(avgQuestion));
+	System.out.println("Score: " + node.evaluate(goodQuestion));
 	System.out.println("Score: " + node.evaluate(incQuestion));
 			
 			
