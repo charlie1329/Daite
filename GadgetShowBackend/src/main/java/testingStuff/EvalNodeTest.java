50632b7e300f72a21868739753bff97c33786866
tauomicronmu <tauomicronmurs@gmail.com>
2016-09-21 22:42:18 +0100

    updated thresholds to 1.0 and 5.0

--- GadgetShowBackend/src/main/java/testingStuff/EvalNodeTest.java
+++ GadgetShowBackend/src/main/java/testingStuff/EvalNodeTest.java
@@ -20,10 +20,12 @@ public class EvalNodeTest
 	String incQuestion = "do you like memes?";
 	String goodQuestion = "how much do you like memes?";
 	String avgQuestion = "what is your opinion on memes?";
+	String mehQuestion = "do you like hitler?";
 	String badQuestion = "how is your pet rhinocerous this fine morning?";
 	Question node = new Question(heldMessage, kWords, false, false, "test", analyser);
 		
 	System.out.println("Score: " + node.evaluate(badQuestion));
+	System.out.println("Score: " + node.evaluate(mehQuestion));
 	System.out.println("Score: " + node.evaluate(avgQuestion));
 	System.out.println("Score: " + node.evaluate(goodQuestion));
 	System.out.println("Score: " + node.evaluate(incQuestion));
