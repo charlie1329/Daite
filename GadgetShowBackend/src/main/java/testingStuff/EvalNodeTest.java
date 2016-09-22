e37dfe2f89f535f7caec4ef3b3eb6cba2d8f1302
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-09-22 11:47:37 +0100

    probably extracted the test method into a function and made it nicer?

--- GadgetShowBackend/src/main/java/testingStuff/EvalNodeTest.java
+++ GadgetShowBackend/src/main/java/testingStuff/EvalNodeTest.java
@@ -12,27 +12,31 @@ public class EvalNodeTest
  {
 	Analyser analyser = new Analyser();
 
+     public static testqs(String h, String q1, String q2, String q3, String q4, String q5, String[] kwords) {
+         Question node = new Question(h, kwords, false, false, "test", analyser);
+         System.out.println("Score: " + node.evaluate(badQuestion));
+         System.out.println("Score: " + node.evaluate(mehQuestion));
+         System.out.println("Score: " + node.evaluate(avgQuestion));
+         System.out.println("Score: " + node.evaluate(goodQuestion));
+         System.out.println("Score: " + node.evaluate(incQuestion));
+ }
+
 	String heldMessage = "do you like memes?";
-	String[] kWords = new String[2];
-	kWords[0] = "memes";
-	kWords[1] = "like";
-	ArrayList<String> neighbours = new ArrayList<String>();
+	String[] kWords = ["memes", "like"];
 	String incQuestion = "do you like memes?";
 	String goodQuestion = "how much do you like memes?";
 	String avgQuestion = "what is your opinion on memes?";
 	String mehQuestion = "do you like hitler?";
 	String badQuestion = "how is your pet rhinocerous this fine morning?";
-	Question node = new Question(heldMessage, kWords, false, false, "test", analyser);
-		
-	System.out.println("Score: " + node.evaluate(badQuestion));
-	System.out.println("Score: " + node.evaluate(mehQuestion));
-	System.out.println("Score: " + node.evaluate(avgQuestion));
-	System.out.println("Score: " + node.evaluate(goodQuestion));
-	System.out.println("Score: " + node.evaluate(incQuestion));
-			
-			
-	
+	testqs(heldMessage,badQuestion,mehQuestion,avgQuestion,goodQuestion,incQuestion, kWords);
+
+     heldMessage = "what do you think about the untimely death of harambe?";
+     kWords = ["think", "death", "harambe"];
+     incQuestion = "what do you think about the untimely death of harambe?";
+	 goodQuestion = "what do you think about harambe's death?";
+     avgQuestion = "how did you feel about harambe's death";
+     mehQuestion = "do you think about death a lot?";
+
+     testqs(heldMessage,badQuestion,mehQuestion,avgQuestion,goodQuestion,incQuestion, kWords);
  }
- 
- 
 }
